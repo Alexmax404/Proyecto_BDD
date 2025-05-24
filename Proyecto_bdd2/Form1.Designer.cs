c66e4d92e679fdc50d5c523d712786c916863004
Glenn Alexander Ward Ante <glennbarsa@outlook.com>
2025-05-24 13:23:50 -0500

    boton acceder de p0.0 a p0.1 funciona

--- Proyecto_bdd2/Form1.Designer.cs
+++ Proyecto_bdd2/Form1.Designer.cs
@@ -28,24 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
+            p0_btn_acceder = new Button();
             SuspendLayout();
             // 
-            // button1
+            // p0_btn_acceder
             // 
-            button1.Location = new Point(348, 376);
-            button1.Name = "button1";
-            button1.Size = new Size(94, 29);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            p0_btn_acceder.Location = new Point(12, 409);
+            p0_btn_acceder.Name = "p0_btn_acceder";
+            p0_btn_acceder.Size = new Size(94, 29);
+            p0_btn_acceder.TabIndex = 0;
+            p0_btn_acceder.Text = "Acceder";
+            p0_btn_acceder.UseVisualStyleBackColor = true;
+            p0_btn_acceder.Click += button1_Click_2;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(button1);
+            Controls.Add(p0_btn_acceder);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
@@ -53,6 +54,6 @@
 
         #endregion
 
-        private Button button1;
+        private Button p0_btn_acceder;
     }
 }
--- Proyecto_bdd2/Form1.cs
+++ Proyecto_bdd2/Form1.cs
@@ -7,16 +7,13 @@ namespace Proyecto_bdd2
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+
+
+        private void button1_Click_2(object sender, EventArgs e)
         {
-
+            this.Hide();               // Oculta el formulario actual (Form1)
+            Form2 form2 = new Form2();
+            form2.Show();              // Muestra Form2
         }
-
-        private void button1_Click_1(object sender, EventArgs e)
-        {
-
-        }
-
-
     }
 }
